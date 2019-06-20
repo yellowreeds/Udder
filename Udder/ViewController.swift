1155eb944cf41f709dce12162f630f7df84f3d33
Aria Bisma Wahyutama <abismaw@gmail.com>
2019-06-20 09:37:37 +0700

    add didreceivememorywarning and viewdidappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,7 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+    }
 
 }
 
