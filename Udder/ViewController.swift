cf5195636c36b433189b2c4cb2385d56cd2bf4b5
Aria Bisma Wahyutama <abismaw@gmail.com>
2019-06-20 09:10:05 +0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Aria Bisma Wahyutama on 20/06/19.
+//  Copyright © 2019 Aria Bisma Wahyutama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
